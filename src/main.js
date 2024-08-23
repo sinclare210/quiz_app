e008f8bc843621bc78ad50bf54f715c7f6d5a478
Sinclare210 <adebayooluwasegun2004@gmail.com>
2024-08-23 05:36:44 -0500

    testing pages rendering because i wasn't liking the outcome when i refresh the page

--- src/main.js
+++ src/main.js
@@ -1,30 +1,30 @@
-const start = document.getElementById("start");
+// const start = document.getElementById("start");
 
-const info = document.getElementById("info");
+// const info = document.getElementById("info");
 
-const exit = document.getElementById("exit");
+// const exit = document.getElementById("exit");
 
-const question = document.getElementById("question");
+// const question = document.getElementById("question");
 
-const cont = document.getElementById("cont");
-
-
-start.addEventListener("click", () => {
-    start.classList.add("hidden");
-    info.classList.remove("hidden");
+// const cont = document.getElementById("cont");
 
 
-});
+// start.addEventListener("click", (event) => {
+   
+//     start.classList.add("hidden");
+//     info.classList.remove("hidden");
 
-exit.addEventListener("click", () => {
-     info.classList.add("hidden");
-      start.classList.remove("hidden");
-})
 
-cont.addEventListener("click", () => {
-    info.classList.add("hidden");
-    question.classList.remove("hidden");
+// });
 
-})
+// exit.addEventListener("click", () => {
+//      info.classList.add("hidden");
+//       start.classList.remove("hidden");
+// })
 
+// cont.addEventListener("click", () => {
+//     info.classList.add("hidden");
+//     question.classList.remove("hidden");
+
+// })
 
