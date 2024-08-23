c47eb4d2a7f4a950b58e4ca3beba3b3f048e5168
Sinclare210 <adebayooluwasegun2004@gmail.com>
2024-08-23 05:08:45 -0500

    done with mobile responsive question

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 const start = document.getElementById("start");
+
 const info = document.getElementById("info");
 
 const exit = document.getElementById("exit");
+
+const question = document.getElementById("question");
+
+const cont = document.getElementById("cont");
+
 
 start.addEventListener("click", () => {
     start.classList.add("hidden");
@@ -14,3 +20,11 @@ exit.addEventListener("click", () => {
      info.classList.add("hidden");
       start.classList.remove("hidden");
 })
+
+cont.addEventListener("click", () => {
+    info.classList.add("hidden");
+    question.classList.remove("hidden");
+
+})
+
+
