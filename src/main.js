451bae98437b7445ca9ff89f1d54f624e1fde28a
Sinclare210 <adebayooluwasegun2004@gmail.com>
2024-08-25 10:34:07 -0500

    perfect

--- src/main.js
+++ src/main.js
@@ -83,18 +83,21 @@ next.addEventListener("click", () => {
 
 function showQuestions(index) {
   que.textContent = `${questions[index]["numb"]}. ${questions[index]["question"]}`;
-  op1.textContent = `${questions[index]["options"][0]}`;
-  op2.textContent = `${questions[index]["options"][1]}`;
-  op3.textContent = `${questions[index]["options"][2]}`;
-  op4.textContent = `${questions[index]["options"][3]}`;
+  // op1.textContent = `${questions[index]["options"][0]}`;
+  // op2.textContent = `${questions[index]["options"][1]}`;
+  // op3.textContent = `${questions[index]["options"][2]}`;
+  // op4.textContent = `${questions[index]["options"][3]}`;
+    arrayOp.forEach((op, i) => {
+    op.textContent = `${questions[index]["options"][i]}`
+  });
 
-
+//remove the bg-green, bg-red,pointer-event-none each time this function is called
   arrayOpt.forEach((opt, i) => {
     opt.classList.remove("bg-green-300", "bg-red-300", "pointer-events-none");
     opt.innerHTML = `<span>${questions[index]["options"][i]}</span>`; 
   });
 
-
+//sends the event of option selected to the optionSelected function
   arrayOpt.forEach((opt) => {
     opt.onclick = () => optionSelected(opt); 
   });
@@ -112,6 +115,9 @@ function optionSelected(answer) {
   let correctAnswer = questions[queCount]["answer"];
   const alloptions = arrayOp.length;
 
+//i used insertAdjacentHTML() method to manipulate the 
+//option to show heck for right answer and cross for wrong ones 
+
   if (userAnswer === correctAnswer) {
     userScore++;
     answer.classList.add("bg-green-300");
@@ -128,13 +134,14 @@ function optionSelected(answer) {
     }
   }
 
-  // Disable all options
+  // Disable all other options one option has been clicked
   arrayOpt.forEach((opt) => {
     opt.classList.add("pointer-events-none");
   });
 
   next.classList.remove("hidden");
 }
+
 //This function shows the user score and comment on what the user got.
 function showResult() {
   question.classList.add("hidden");
@@ -153,17 +160,20 @@ function startTimer(time) {
   function timer() {
     timeLeft.textContent = time; 
     time--;
+// when timer is less than zero i automatically loops 
+//through the option to pick the answer
     if (time < 0) {
       clearInterval(counter);
       const alloptions = arrayOp.length;
       let correctAnswer = questions[queCount]["answer"];
-
+//time() method is used to remove whiespaces
       for (let i = 0; i < alloptions; i++) {
         if (arrayOp[i].textContent.trim() === correctAnswer) {
           arrayOpt[i].classList.add("bg-green-300");
           arrayOpt[i].insertAdjacentHTML("beforeend", tickIconTag);
         }
       }
+ //this part of the code disable sending of click event when the time is up     
       arrayOpt.forEach((opt) => opt.classList.add("pointer-events-none"));
       next.classList.remove("hidden");
     }
